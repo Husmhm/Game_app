a8ad2f2a6aec93b92df9fe3aaae83ccc247f0637
Husmhm <hu3einmhm@gmail.com>
2024-06-15 21:50:32 +0330

    add password to user entity & hash it with bycript

--- entity/user.go
+++ entity/user.go
@@ -4,6 +4,5 @@ type User struct {
 	ID          uint
 	PhoneNumber string
 	Name        string
-	//CreatedAt   time.Time
-	//UpdatedAt   time.Time
+	Password    string
 }
--- service/userservice/service.go
+++ service/userservice/service.go
@@ -1,9 +1,11 @@
 package userservice
 
 import (
+	"encoding/hex"
 	"fmt"
 	"gameApp/entity"
 	"gameApp/pkg/phonenumber"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Repository interface {
@@ -18,6 +20,7 @@ type Service struct {
 type RegisterRequest struct {
 	Name        string `json:"name"`
 	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
 }
 
 type RegisterResponse struct {
@@ -32,7 +35,7 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 	// TODO - We should verify phone number by verification code
 	//validate phone number
 	if !phonenumber.IsValid(req.PhoneNumber) {
-		return RegisterResponse{}, fmt.Errorf("phone number is`nt valid")
+		return RegisterResponse{}, fmt.Errorf("phone number isnt valid")
 	}
 
 	// check uniqueness of phone number
@@ -51,14 +54,27 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 
 	// validate name
 	if len(req.Name) < 3 {
-		return RegisterResponse{}, fmt.Errorf("name is`nt valid")
+		return RegisterResponse{}, fmt.Errorf("name isnt valid")
 	}
 
+	// TODO - chevk the password with regex pattern
+	// validate password
+	if len(req.Password) < 8 {
+		return RegisterResponse{}, fmt.Errorf("password lentgth should be greater than 8")
+	}
 	// created new user in storage
+	pass := []byte(req.Password)
+	hash, err := bcrypt.GenerateFromPassword(pass, bcrypt.DefaultCost)
+	hashStr := hex.EncodeToString(hash)
+	if err != nil {
+		panic(err)
+	}
+
 	user := entity.User{
 		ID:          0,
 		PhoneNumber: req.PhoneNumber,
 		Name:        req.Name,
+		Password:    hashStr,
 	}
 	createdUser, err := s.repo.Register(user)
 	if err != nil {
@@ -69,4 +85,23 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 	return RegisterResponse{
 		User: createdUser,
 	}, nil
+}
+
+type LoginRequest struct {
+	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
+}
+
+type LoginResponse struct {
+}
+
+func (s Service) Login(req RegisterRequest) (RegisterResponse, error) {
+	// check existence phone number from repository
+
+	// get the user by phone number
+
+	// compare user.Password with the req.Password
+
+	// return ok
+	panic("implement me")
 }
